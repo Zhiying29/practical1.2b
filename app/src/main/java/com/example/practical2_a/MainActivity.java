43efc495ca3749ff702219e50093c2576df1c60c
Zhiying29 <66941589+Zhiying29@users.noreply.github.com>
2020-07-03 12:25:24 +0800

    commit by Soh Zhi Ying. "Hello Constraint".

---
+++ app/src/main/java/com/example/practical2_a/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.practical2_a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view){
+        Toast toast = Toast.makeText(this,"Soh Zhi Ying 19WMD03135",Toast.LENGTH_SHORT);
+
+        toast.show();
+    }
+
+    public void countUp(View view){
+        ++mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void resetCount(View view){
+        mCount=0;
+        mShowCount.setText(String.valueOf(mCount));
+        if(mShowCount !=null) {
+            mShowCount.setText(Integer.toString(mCount));
+            int number = mCount % 2;
+            if (number ==0){
+                view.setBackgroundColor(Color.GREEN);
+            }
+            else
+                view.setBackgroundColor(Color.RED);
+            findViewById(R.id.button_zero).setBackgroundColor(Color.RED);
+        }
+    };
+    }
